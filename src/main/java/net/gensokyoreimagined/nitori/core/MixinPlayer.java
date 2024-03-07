9846396345763d2e4f546f73e77f593e4e673e27
DoggySazHi <doggysazhi@gmail.com>
2024-03-06 21:07:29 -0800

    ??? (implemented 0064-Smarter-statistics-ticking.patch)

---
+++ src/main/java/net/gensokyoreimagined/nitori/core/MixinPlayer.java
@@ -0,0 +1,41 @@
+// Nitori Copyright (C) 2024 Gensokyo Reimagined
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+package net.gensokyoreimagined.nitori.core;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Player.class)
+public abstract class MixinPlayer extends Entity {
+    public MixinPlayer(Level world) {
+        super(EntityType.PLAYER, world);
+    }
+
+    @Shadow public abstract void awardStat(ResourceLocation stat, int amount);
+
+    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;awardStat(Lnet/minecraft/resources/ResourceLocation;)V"))
+    public void betterStatsTicking(Player instance, ResourceLocation stat) {
+        if (this.tickCount % 20 == 0) {
+            this.awardStat(stat, 20);
+        }
+    }
+}

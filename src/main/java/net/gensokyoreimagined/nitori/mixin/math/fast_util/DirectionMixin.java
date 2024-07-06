a9be79a6a000b1f0a874b8b539a3ccaf1aac0cf1
Taiyou06 <kaandindar21@gmail.com>
2024-07-07 02:28:22 +0300

    faster methods

---
+++ src/main/java/net/gensokyoreimagined/nitori/mixin/math/fast_util/DirectionMixin.java
@@ -0,0 +1,38 @@
+//package net.gensokyoreimagined.nitori.mixin.math.fast_util;
+//
+//import net.minecraft.core.Direction;
+//import net.minecraft.core.Direction.Axis;
+//import net.minecraft.util.RandomSource;
+//import org.spongepowered.asm.mixin.Final;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.Overwrite;
+//import org.spongepowered.asm.mixin.Shadow;
+//
+//@Mixin(Direction.class)
+//public class DirectionMixin {
+//    @Shadow
+//    @Final
+//    private static Direction[] ALL;
+//
+//    @Shadow
+//    @Final
+//    private int idOpposite;
+//
+//    /**
+//     * @reason Avoid the modulo/abs operations
+//     * @author JellySquid
+//     */
+//    @Overwrite
+//    public Direction getOpposite() {
+//        return ALL[this.idOpposite];
+//    }
+//
+//    /**
+//     * @reason Do not allocate an excessive number of Direction arrays
+//     * @author JellySquid
+//     */
+//    @Overwrite
+//    public static Direction getRandom(RandomSource rand) {
+//        return Direction.ALL[rand.nextInt(ALL.length)];
+//    }
+//}

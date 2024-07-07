0dbf37590607b555bc3a4fe09eb9a867f69fdd0e
Taiyou06 <kaandindar21@gmail.com>
2024-07-07 04:11:54 +0300

    remove unneeded stuff
    - remove BoxMixin as it's the same with MixinAABB (lmao)
    - refactor MixinAABB's loaction
    - disable problematic mixins for now

--- build-logic/src/main/kotlin/mod.base-conventions.gradle.kts
+++ build-logic/src/main/kotlin/mod.base-conventions.gradle.kts
@@ -3,7 +3,6 @@ plugins {
 
   id("io.github.goooler.shadow")
   id("io.papermc.paperweight.userdev")
-  id("com.diffplug.spotless")
   id("maven-publish")
 }
 
@@ -38,49 +37,4 @@ tasks {
   build {
     dependsOn(reobfJar)
   }
-}
-
-spotless {
-  format("misc") {
-    target(project.files("*.gradle.kts", "gradle.properties", "settings.gradle.kts", "gradle/libs.versions.toml"))
-
-    trimTrailingWhitespace()
-    indentWithSpaces(4)
-    endWithNewline()
-  }
-
-  java {
-    licenseHeaderFile("LICENSE_header.txt")
-  }
-}
-
-var jarFile = file("build/libs/%s-%s.jar".format(project.name, project.version))
-var jarArtifact = artifacts.add("default", jarFile) {
-  type = "jar"
-  builtBy("jar")
-}
-
-publishing {
-  publications {
-    create<MavenPublication>("mavenJava") {
-      artifact(jarArtifact)
-      group = "plugins"
-    }
-  }
-
-  repositories {
-    maven {
-      name = "gensorepo"
-      credentials {
-        username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-        password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-      }
-      // url to the releases maven repository
-      url = uri("https://repo.gensokyoreimagined.net/")
-    }
-  }
-}
-
-tasks.named("publishMavenJavaPublicationToGensorepoRepository") {
-  dependsOn("reobfJar")
 }

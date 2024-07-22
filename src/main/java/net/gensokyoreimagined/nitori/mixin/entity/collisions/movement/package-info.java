59422498c4a7da99b6af2fdf999b1e801a19a58e
Taiyou06 <kaandindar21@gmail.com>
2024-07-22 11:30:39 +0300

    faster predicates, faster angles etc...

---
+++ src/main/java/net/gensokyoreimagined/nitori/mixin/entity/collisions/movement/package-info.java
@@ -0,0 +1,5 @@
+package net.gensokyoreimagined.nitori.mixin.entity.collisions.movement;
+
+/*
+TODO: https://github.com/CaffeineMC/lithium-fabric/blob/3bdbb553b47165bdbd8c3ca0807bb374a19ce04a/src/main/java/me/jellysquid/mods/lithium/mixin/entity/collisions/movement/EntityMixin.java
+ */
